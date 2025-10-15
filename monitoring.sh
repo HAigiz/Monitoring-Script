19767ae1ac5e1a3008002d6f8ee1432d46b7cadf
Aygiz <khafizov.aygiz.i@bk.ru>
2025-10-15 21:07:36 +0300

    Update Unit's & Scripts

--- monitoring.sh
+++ monitoring.sh
@@ -1,35 +1,21 @@
 #!/bin/bash
 
-LOG_FILE="/var/log/monitoring.log"
 PROCESS_NAME="test"
-MONITORING_URL="https://test.com/monitoring/test/api"
-PID_FILE="/var/run/${PROCESS_NAME}_monitoring.pid"
+MONITORING_URL="http://192.168.11.154"
 # ----------------------------------------------------
 
-log_message() {
-	echo "$(date '+%Y-%m-%d %H:%M:%S') - $1" >> "$LOG_FILE"
-}
 
-CURRENT_PID=$(ps aux | grep "$PROCESS_NAME".service | awk '{print $2}')	#pgrep не получилось поэтому использую такую большую команду
+CURRENT_PID=$(ps aux | grep "$PROCESS_NAME".service | awk '{print $2}') #pgrep не получилось поэтому использую такую большую команду
 if [ -n "$CURRENT_PID" ]; then
-	if [ -f "$PID_FILE" ]; then
-		LAST_PID=$(cat "$PID_FILE")
-		if [ "$CURRENT_PID" != "$LAST_PID" ]; then
-			log_message "INFO: Process '$PROCESS_NAME' was restarted."
-		fi
-	fi
-	echo "$CURRENT_PID" > "$PID_FILE"
 
-	HTTPS_STATUS=$(curl -s -o /dev/null -w "%{http_code}" "$MONITORING_URL")
-	CURL_EXIT_CODE=$?
-	
-	if [ $CURL_EXIT_CODE -ne 0 ]; then
-		log_message "ERROR: Monitoring server is unavailable."
-	elif [ "$HTTPS_STATUS" -ge 400 ]; then
-		log_message "WARNING: Monitoring server returned error status code: $HTTPS_STATUS."
-	fi
+        HTTP_STATUS=$(curl -s -o /dev/null -w "%{http_code}" "$MONITORING_URL")
+        CURL_EXIT_CODE=$?
+
+        if [ $CURL_EXIT_CODE -ne 0 ]; then
+                echo "$(date '+%Y-%m-%d %H:%M:%S') CRITICAL: Сервер мониторинга недоступен (curl exit code $CURL_EXIT_CODE)." >&2
+        elif [ "$HTTP_STATUS" -ge 400 ]; then
+                echo "$(date '+%Y-%m-%d %H:%M:%S') WARNING: API вернул код ошибки $HTTP_STATUS." >&2
+        fi
 else
-	if [ -f "$PID_FILE" ]; then
-		rm "$PID_FILE"
-	fi
+        echo "$(date '+%Y-%m-%d %H:%M:%S') INFO: Процесс $PROCESS_NAME не запущен. Проверка API пропущена."
 fi
